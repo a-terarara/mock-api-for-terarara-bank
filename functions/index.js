a95c2125e8bd493d8b3298c7dc8484a06a4c2418
terahara.ayumu@gmail.com <terahara ayumu>
2018-09-30 18:57:59 +0900

    [mod] error send of json.

--- functions/index.js
+++ functions/index.js
@@ -4,25 +4,25 @@ const cors = require("cors")({ origin: true });
 exports.accounts = https.onRequest((req, res) => {
   cors(req, res, () => {});
   if (!verifyAccessToken(req.headers.authorization))
-    res.status(403).send("Unauthorized");
+    res.status(403).send({ error: "Unauthorized" });
 
   fetchAccounts(res);
 });
 exports.transactions = https.onRequest((req, res) => {
   cors(req, res, () => {});
   if (!verifyAccessToken(req.headers.authorization))
-    res.status(403).send("Unauthorized");
+    res.status(403).send({ error: "Unauthorized" });
 
   fetchTransactions(req.query.account_number, res);
 });
 exports.transfer = https.onRequest((req, res) => {
   cors(req, res, () => {});
   if (!verifyAccessToken(req.headers.authorization))
-    res.status(403).send("Unauthorized");
+    res.status(403).send({ error: "Unauthorized" });
   if (req.method === "POST") createTransfer(req.body, res);
   if (req.method === "PUT") confirmTransfer(req.body, res);
 
-  res.status(405).send("Method Not Allowed");
+  res.status(405).send({ error: "Method Not Allowed" });
 });
 
 verifyAccessToken = authz => {
@@ -41,9 +41,9 @@ fetchAccounts = res => {
   });
 };
 fetchTransactions = (account_number, res) => {
-  if (!account_number) return res.status(400).send("Invalid value");
+  if (!account_number) return res.status(400).send({ error: "Invalid value" });
   if (account_number !== 12345)
-    return res.status(404).send("Account Not found");
+    return res.status(404).send({ error: "Account Not found" });
 
   return res.send([
     {
@@ -84,15 +84,15 @@ createTransfer = (body, res) => {
       amount
     )
   )
-    return res.status(400).send("Invalid value");
+    return res.status(400).send({ error: "Invalid value" });
 
   return res.send({ transfer_id: "12345" });
 };
 confirmTransfer = (body, res) => {
   const { transfer_id } = body;
-  if (!transfer_id) return res.status(400).send("Invalid value");
+  if (!transfer_id) return res.status(400).send({ error: "Invalid value" });
   if (transfer_id !== "12345")
-    return res.status(404).send("Transfer not found");
+    return res.status(404).send({ error: "Transfer not found" });
 
   return res.send({ transfer_id: "12345" });
 };
